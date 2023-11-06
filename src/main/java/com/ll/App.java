bfa260599812e5fbf5b01d9d0564212e834e729a
i_seoyeon <tus0926@naver.com>
2023-11-06 15:11:04 +0900

    등록 메서드 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -19,6 +19,13 @@ public class App {
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언: ");
+                String content = scanner.nextLine();
+                System.out.print("작가: ");
+                String authorName = scanner.nextLine();
+                System.out.println("명언이 등록되었습니다.");
+            }
         }
     }
 }
