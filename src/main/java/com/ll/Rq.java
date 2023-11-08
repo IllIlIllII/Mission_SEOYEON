ecb5bcf92514c953adca26b82f0a0c28e5b5ef84
i_seoyeon <tus0926@naver.com>
2023-11-08 14:17:41 +0900

    cmd 해체 작업을 맡는 Rq 클래스 생성 2

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,14 +1,22 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rq {
     String cmd;
     String action;
     String queryString;
+    List<String> paramNames;
+    List<String> paramValues;
 
     Rq(String cmd) {
+        paramNames = new ArrayList<>();
+        paramValues = new ArrayList<>();
+
         this.cmd = cmd;
 
-        String[] cmdBits = cmd .split("\\?", 2);
+        String[] cmdBits = cmd.split("\\?", 2);
         action = cmdBits[0];
         queryString = cmdBits[1];
 
@@ -16,10 +24,33 @@ public class Rq {
 
         for (int i = 0; i < queryStringBits.length; i++) {
             String queryParamStr = queryStringBits[i];
-            String[] queryParamStrBits = queryParamStr.split("=");
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
 
             String paramName = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
+
+            paramNames.add(paramName);
+            paramValues.add(paramValue);
+        }
+    }
+
+    String getAction() {
+        return action;
+    }
+
+    public int getParamAsInt(String paramName, int defaultValue) {
+        int index = paramValues.indexOf(paramName);
+
+        if (index == -1) {
+            return defaultValue;
+        }
+
+        String paramValue = paramValues.get(index);
+
+        try {
+            return Integer.parseInt(paramValue);
+        } catch (NumberFormatException e) {
+            return defaultValue;
         }
     }
 }
